4e989a7ecccfe55d06e1baecb18108e2a3b947b4
James English <james.t.english@saic.com>
2019-03-04 22:59:56 -0500

    Updates

---
+++ precache-manifest.da4b8526af6515d9689cd96bf03d7f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "95f56e52d11407392ec4",
+    "url": "/simple-number-eye-baller/static/js/runtime~main.95f56e52.js"
+  },
+  {
+    "revision": "c76f8ebb3ef0d7679963",
+    "url": "/simple-number-eye-baller/static/js/main.c76f8ebb.chunk.js"
+  },
+  {
+    "revision": "6fd0231800df314ee011",
+    "url": "/simple-number-eye-baller/static/js/2.6fd02318.chunk.js"
+  },
+  {
+    "revision": "c76f8ebb3ef0d7679963",
+    "url": "/simple-number-eye-baller/static/css/main.0c057ffb.chunk.css"
+  },
+  {
+    "revision": "6fd0231800df314ee011",
+    "url": "/simple-number-eye-baller/static/css/2.62196667.chunk.css"
+  },
+  {
+    "revision": "ad7d0f5a5c66f63b3662541a112b180f",
+    "url": "/simple-number-eye-baller/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eeb7961aa5ca42894e9af13c68f79d34.js"
+  "/simple-number-eye-baller/precache-manifest.da4b8526af6515d9689cd96bf03d7f5c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/simple-number-eye-baller/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
